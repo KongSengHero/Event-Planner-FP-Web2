d97e969a77a3bab248df96992abfbaeba22d3995
Kong Seng <KongSengHero@gmail.com>
2026-06-09 14:17:43 +0700

    Template Setup 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,7 @@
 import { Routes, Route } from 'react-router-dom'; 
+import Home from './pages/Home'; 
+import About from './pages/About'; 
+import Contact from './pages/Contact'; 
 
 
 
@@ -7,8 +10,9 @@ import { Routes, Route } from 'react-router-dom';
 export default function App() { 
     return ( 
         <Routes>
-            <Route path="/" element={<h1>Home</h1>} />
-            <Route path="/lobby" element={<h1>Lobby</h1>} />
+            <Route path="/" element={ <Home /> } />
+            <Route path="/contact" element={ <Contact /> } />
+            <Route path="/about" element={ <About /> } />
         </Routes>
     )
 }
