d0a52d0d63851672b1ec248fdb61645183fb17c6
LiebeandSkye <kryrithisak@gmail.com>
2026-06-30 14:46:00 +0700

    environment for testing

---
+++ frontend/src/AppPreview.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './pages/Home'
+/*this is for rendering the app to preview UI only*/
+const AppPreview = () => {
+    return (
+        <div>
+            <Home />
+        </div>
+    )
+}
+
+export default AppPreview
