375e5b9997548e0d749556b89e261e833744a001
LiebeandSkye <kryrithisak@gmail.com>
2026-06-30 15:13:10 +0700

    update header UI and dashboard section

--- frontend/src/components/NavigationHeader.jsx
+++ frontend/src/components/NavigationHeader.jsx
@@ -7,13 +7,17 @@ import { NavLink } from 'react-router-dom';
 
 export default function NavigationHeader() { 
     return ( 
-        <div>
-            <NavLink to="/">Login</NavLink>
-            <NavLink to="/home">Home</NavLink>
-            <NavLink to="/dashboard">Dashboard</NavLink>
-            <NavLink to="/global_event">Global Event</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
-            <NavLink to="/about">About</NavLink>
+        <div className='px-16 py-5 items-center justify-between flex gap-5 flex-row border-b border-gray-500 bg-neutral-900'>
+            <div className='flex gap-10 items-center'>
+                <NavLink className='text-white hover:text-blue-500 text-xl' to="/home">Home</NavLink>
+                <NavLink className='text-white hover:text-blue-500 text-xl' to="/dashboard">Dashboard</NavLink>
+                <NavLink className='text-white hover:text-blue-500 text-xl' to="/global_event">Global Event</NavLink>
+                <NavLink className='text-white hover:text-blue-500 text-xl' to="/contact">Contact</NavLink>
+                <NavLink className='text-white hover:text-blue-500 text-xl' to="/about">About</NavLink>
+            </div>
+            <div>
+                <NavLink className='btn' to="/">Login</NavLink>
+            </div>
         </div>
     )
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 
 
 
+// Thepy part to manage and create events. After clicking create it should display cards of the event that you created.
 
+// fyi when clicking create it should redirect to a form where you input your information and should display at dashboard as cards.
 
 export default function Dashboard() { 
     return ( 
-        <div>Dashboard</div>
+        <div>
+            <div className="mt-10 flex flex-row justify-between items-center px-20">
+                <div className="text-3xl font-semibold">Dashboard</div>
+                <div><button className='btn'>Create</button></div>
+            </div>
+        </div>
     )
 }
